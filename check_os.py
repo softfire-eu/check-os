6be697244243ac931532c32c0a656146f9b0a83d
Sarjo Das <sarjo.das@fokus.fraunhofer.de>
2017-11-22 20:09:26 +0100

    Image Search Update - Ongoing

--- check_os.py
+++ check_os.py
@@ -1,10 +1,43 @@
 import argparse
 import json
 import logging.config
+import configparser
+
+from keystoneauth1.exceptions import Unauthorized
 
 from sdk.softfire.os_utils import OSClient
 
 log = logging.getLogger(__name__)
+
+def config_script():
+    print("Reading image list...")
+    config_script = configparser.ConfigParser()
+    config_script.read("./list_images.ini")
+    image_list = config_script.get('images', 'imagesToCheck')
+    image_list = image_list.split(',')
+    return image_list
+
+
+def search_images(testbeds):
+    image_list = config_script()
+    for testbed_name, testbed in testbeds.items():
+        cl = OSClient(testbed_name=testbed_name, testbed=testbed)
+        log.info("Checking Testbed %s" % testbed_name)
+        log.info("Tenant List:")
+        for project in cl.list_tenants():
+            try:
+                log.info("Checking Project %s" % project.name)
+                log.info("Checking project %s" % project.id)
+                images = cl.list_images(project.id)
+                for img in images:
+                    for list in image_list[0:5]:
+                        if img.name == list:
+                            log.info("Success matched image %s", list)
+                        elif img.name != list:
+                            log.info("Need to upload image %s", list)
+                #log.debug([img.name for img in images])
+            except Unauthorized:
+                log.warning("Not authorized on project %s" % project.name)
 
 if __name__ == '__main__':
     logging.config.fileConfig("etc/logging.ini", disable_existing_loggers=False)
@@ -13,13 +46,9 @@ if __name__ == '__main__':
                         default='/etc/softfire/openstack-credentials.json')
     args = parser.parse_args()
 
-    openstack_credentials = args.os_cred
+    openstack_credentials = args.os_cred #"/etc/softfire/openstack-credentials.json"
     testbeds = {}
     with open(openstack_credentials, "r") as f:
         testbeds = json.loads(f.read())
 
-    for testbed_name, testbed in testbeds.items():
-        cl = OSClient(testbed_name=testbed_name, testbed=testbed)
-        log.info("Checking Testbed %s" % testbed_name)
-        for project in cl.list_tenants():
-            log.info("Checking Project %s" % project.name)
+    search_images(testbeds)
